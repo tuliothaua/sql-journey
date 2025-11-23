ef6678bb33e09272020a50bfe2a1bf9bed482a2f
Túlio Thauã Dutra <tuliothauandutra@gmail.com>
2025-11-23 18:46:37 -0300

    feat: add sales report queries for employees and products
    It add sales report queries for employees and products

--- MySQL/consultas_boteco.sql
+++ MySQL/consultas_boteco.sql
@@ -9,3 +9,20 @@ join produtos p on p.id_produto = ip.produto_id
 group by p.nome_produto
 order by total_vendido desc
 limit 5;
+
+-- Relatorio de vendas por funcionario
+#Produtos
+select f.nome_funcionario, sum(ip.qtde * ip.valor_unitario) as total
+from funcionarios f
+join itens_produtos ip on ip.funcionario_id = f.id_funcionario
+group by f.nome_funcionario;
+
+#Lanches
+select f.nome_funcionario, sum(il.qtde * il.valor_unitario) as total
+from funcionarios f join itens_lanches il on il.funcionario_id = f.id_funcionario
+group by f.nome_funcionario;
+
+#Drinks
+select f.nome_funcionario, sum(itd.qtde * itd.valor_unitario) as total
+from funcionarios f join itens_drinks itd on itd.funcionario_id = f.id_funcionario
+group by f.nome_funcionario;
